0687b6b87f1f97ab06037fe5f7d8c88a492c6368
Tinsae <tigial3535@gmail.com>
2021-03-21 02:25:50 -0500

    PART 7 - Firebase Login and SignUp

---
+++ app/src/main/java/org/tproj/ecommerce1/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package org.tproj.ecommerce1.Prevalent;
+
+import org.tproj.ecommerce1.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
